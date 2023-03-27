5b3b3d9af28014d3a5e3ff215100a1b6229684cd
Zetrinjin <poleev.nikita@mail.ru>
2023-03-27 21:15:26 +0300

    task1 done

--- src/lesson6/ArrayExample.java
+++ src/lesson6/ArrayExample.java
@@ -1,5 +1,7 @@
 package lesson6;
 
+import java.util.Arrays;
+
 public class ArrayExample {
     public static void main(String[] args) {
         int[] arr1;
@@ -8,11 +10,17 @@ public class ArrayExample {
         arr1 =new int[5];// we know size
 
         //arr2 = {1,2,3,4,5} так делать нельзя тк выделятся память
-        int[] arr2 ={1,2,3,4,5};
+        int[] arr2 ={5,2,3,1,4};
 
         for (int i = 0; i< arr1.length;i++){
             System.out.println(arr1[i]);
         }
+        for (int i = 0; i< arr2.length;i++){
+            System.out.print(arr2[i] + " ");
+        }
+
+        System.out.println();
+        Arrays.sort(arr2);
         for (int i = 0; i< arr2.length;i++){
             System.out.println(arr2[i]);
         }
